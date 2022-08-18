64238a3b36c138b47ec127fe2002764ed9a300a7
Eddie Osmanbasic <eddieosmanbasic@rocketmortgage.com>
2022-08-18 15:54:36 -0400

    adding files

---
+++ week2_lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿Dictionary<string, decimal> menu = new Dictionary<string, decimal>();
+menu.Add("apple\t", 0.99m);
+menu.Add("banana\t", 0.59m);
+menu.Add("cauliflower", 1.59m);
+menu.Add("dragonfruit", 2.19m);
+menu.Add("elderberry", 1.79m);
+menu.Add("figs\t", 2.09m);
+menu.Add("grapefruit", 1.99m);
+menu.Add("honeydew", 3.49m);
+
+List<string> cart = new List<string>();
+Console.WriteLine("Welcome to the market!");
+bool redisplayFlag = true;
+while (redisplayFlag == true)
+{
+    Console.WriteLine("Item\t\tPrice");
+    Console.WriteLine("====================================");
+
+    foreach (KeyValuePair<string, decimal> item in menu)
+    {
+        Console.WriteLine($"{item.Key}\t{item.Value}");
+    }
+    bool validEntryFlag = false;
+    while (validEntryFlag == false)
+    {
+        Console.WriteLine("What item would you like to order?");
+        string itemEntry = Console.ReadLine().ToLower();
+        if (itemEntry == null || itemEntry.Length == 0 || menu.ContainsKey(itemEntry) == false)
+        {
+            Console.WriteLine("That was not a valid entry.");
+            Console.WriteLine("Please enter a valid item from ");
+            validEntryFlag = false;
+        }
+        else if (menu.ContainsKey(itemEntry))
+        {
+            validEntryFlag = true;
+            cart.Add(itemEntry);
+        }
+
+
+    }
+
+
+
+}
